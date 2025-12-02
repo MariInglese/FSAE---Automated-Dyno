b84a4582d5638a726a89d1ca7c8ff4b76ea9f0d0
PantherRacing <nstarace8@gmail.com>
2025-12-02 14:41:04 -0500

    100 Hz except error when parsing between values

--- Regression Code/sending_from_csv.py
+++ Regression Code/sending_from_csv.py
@@ -1,16 +1,13 @@
 # Importing libraries 
 import csv, os, time, math, can, cantools
 import cantools.database 
-from CANBase import CANInterface
-from message_data import MessageData
 from can.interfaces.pcan import pcan
 
-file_path = r'fake_csv.csv'
+# First column time
+# Second column torque
+file_path = r'real_csv.csv'
 
 #open and read csv file
-
-from message_data import MessageData
-
 dbc_directory = os.path.join(os.getcwd(), r'hv500_can2_map_v25_SID.dbc')
 dbc_file = dbc_directory
 db = cantools.database.load_file(dbc_file)
@@ -23,17 +20,33 @@ with can.Bus(interface='pcan',
             receive_own_messages=False) as bus:
 
     with open(file_path, mode='r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-
+        reader1 = csv.reader(csvfile)
+        # Get time in between samples 
+        first_row = next(reader1)
+        print(first_row)
+        second_row = next(reader1) 
+        print(second_row)
+        sample_time  = float(second_row[0]) - float(first_row[0])
+        #print(sample_time)
+    with open(file_path, mode='r', newline='') as csvfile:
+        reader2 = csv.reader(csvfile)
         # Iterate through CSV
-        for row in reader: 
-            torque = float(row[1])              # Fetch torque feedback
-            print(torque)
-            data = message.encode({'CMD_TargetAcCurrent': torque})  # Encode message
-            tx_message = can.Message(arbitration_id=0x036, is_extended_id=False,
-                        data=data)                # Formulate message
-            bus.send(tx_message, timeout=0.2)      # Send message to BUS BUS BUS
-            if tx_message is not None:
-                print(f"{tx_message.arbitration_id:X}: {tx_message.data}")
+        for row in reader2: 
+            try: 
+                torque = float(row[1])              # Fetch torque feedback
+                print(torque)
+                data = message.encode({'CMD_TargetAcCurrent': torque})  # Encode message
+                tx_message = can.Message(arbitration_id=0x36, is_extended_id=False,
+                            data=data)                # Formulate message
+                bus.send(tx_message, timeout=0.2)      # Send message to BUS BUS BUS
+                if tx_message is not None:
+                    print(f"{tx_message.arbitration_id:X}: {tx_message.data}")
+  
+                time.sleep(sample_time)                       # Wait for 1s before updating
+            except: 
+                print("in except")
+                continue
                 
-            time.sleep(1)                       # Wait for 1s before updating
+
+
+            
