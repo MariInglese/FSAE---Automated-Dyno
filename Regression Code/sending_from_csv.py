8817f2c9ed5ffbe12544743113e65f8e4ef4707f
PantherRacing <nstarace8@gmail.com>
2026-01-20 17:28:26 -0500

    added ramp csv

--- Regression Code/sending_from_csv.py
+++ Regression Code/sending_from_csv.py
@@ -12,11 +12,11 @@ numMissing = 0
 toggle = 0 
 
 #open and read csv file
-dbc_directory = os.path.join(os.getcwd(), r'hv500_can2_map_v25_SID.dbc')
+dbc_directory = os.path.join(os.getcwd(), r"C:\\GitHub\\FSAE---Automated-Dyno\DBC Tools\\20230411_Gen5_CAN_DBC_PantherRacing_2024_01_15.dbc")
 dbc_file = dbc_directory
 db = cantools.database.load_file(dbc_file)
 
-message = db.get_message_by_name('HV500_SetAcCurrent')
+message = db.get_message_by_name('M192_Command_Message')
 
 with can.Bus(interface='pcan',
             channel='PCAN_USBBUS1',
@@ -42,16 +42,18 @@ with can.Bus(interface='pcan',
                 torque = float(row[1])              # Fetch torque feedback
                 #print(torque)
                 
-                data = message.encode({'CMD_TargetAcCurrent': torque})  # Encode message
-                tx_message = can.Message(arbitration_id=0x36, is_extended_id=False,
+                data = message.encode({'VCU_INV_Torque_Command': torque,'VCU_INV_Speed_Command': 0.0,'VCU_INV_Direction_Command': 1,'VCU_INV_Inverter_Enable': 1,'VCU_INV_Inverter_Discharge': 1,'VCU_INV_Speed_Mode_Enable': 0,'VCU_INV_Torque_Limit_Command': 100.0},strict=False)  # Encode message
+                
+                tx_message = can.Message(arbitration_id=0xC0, is_extended_id=False,
                             data=data)                # Formulate message
                 bus.send(tx_message, timeout=0.2)      # Send message to BUS BUS BUS
                 if tx_message is not None:
                     pass
                     #print(f"{tx_message.arbitration_id:X}: {tx_message.data}")
   
-                #time.sleep(0.001)                       # Wait for 1s before updating
-            except Exception: 
+                time.sleep(0.001)                       # Wait for 1s before updating
+            except Exception as e:
+                print(f"Exception: {e}") 
                 if(toggle < 10): 
                     toggle = toggle +1 
                     print(f"{numMissing}")
